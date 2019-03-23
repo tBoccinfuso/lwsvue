f1e23b4a9b8e04a95bfe5ad1eb6764b2a2153d1f
Thomas Boccinfuso <tboccinfuso@gmail.com>
2019-03-23 14:18:05 -0400

    Added comment to explain vue config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+// Below is an example to extend webpack so all our of css from the styles folder can be used without adding imports into components.
+
 module.exports = {
   css: {
     loaderOptions: {
